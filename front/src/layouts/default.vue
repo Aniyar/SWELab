c25c7d8eac7fbee08cec47644f837582523def5f
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-11-24 23:18:55 +0600

    Front

---
+++ front/src/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+<!--  <div class="flex flex-column h-screen">-->
+<!--    <div class="flex flex-column">-->
+<!--      <router-view v-slot="{Component}">-->
+<!--        <component :is="Component"/>-->
+<!--      </router-view>-->
+<!--    </div>-->
+<!--    <div class="flex flex-column">-->
+<!--      <v-tabs class="bg-semi-light" fixed-tabs>-->
+<!--        <v-tab :ripple="false" value="following" class="text-none">Events</v-tab>-->
+<!--        <v-tab :ripple="false" value="club" class="text-none">+</v-tab>-->
+<!--        <v-tab :ripple="false" value="open" class="text-none">Profile</v-tab>-->
+<!--      </v-tabs>-->
+<!--    </div>-->
+<!--  </div>-->
+</template>
+
+<script lang="ts" setup>
+</script>
